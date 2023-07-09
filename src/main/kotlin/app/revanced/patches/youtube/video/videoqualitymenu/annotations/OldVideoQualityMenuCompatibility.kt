f763122ab3cc58f63e30fe53fa52b6bbd73aa95f
Mafiadoener36 <Mafiadoener36@users.noreply.github.com>
2023-07-09 02:04:10 +0000

    Automated debugMerge

--- src/main/kotlin/app/revanced/patches/youtube/video/videoqualitymenu/annotations/OldVideoQualityMenuCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/youtube/video/videoqualitymenu/annotations/OldVideoQualityMenuCompatibility.kt
@@ -1,8 +1,8 @@
-package app.revanced.patches.youtube.video.speed
+package app.revanced.patches.youtube.video.videoqualitymenu.annotations
 
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
 @Compatibility([Package("com.google.android.youtube", arrayOf("18.20.39", "18.23.35"))])
 @Target(AnnotationTarget.CLASS)
-internal annotation class VideoSpeedCompatibility
+internal annotation class OldVideoQualityMenuCompatibility
