383fee4bc8fad92d86cbf1ccf6b54852766b3066
Mafiadoener36 <Mafiadoener36@users.noreply.github.com>
2024-02-11 01:18:44 +0000

    Automated debugMerge

--- src/main/kotlin/app/revanced/patches/twitch/debug/fingerprints/ShouldShowDebugOptionsFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/twitch/debug/fingerprints/ShouldShowDebugOptionsFingerprint.kt
@@ -1,10 +1,9 @@
 package app.revanced.patches.twitch.debug.fingerprints
 
+import app.revanced.patcher.fingerprint.MethodFingerprint
 
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
-
-object ShouldShowDebugOptionsFingerprint : MethodFingerprint(
-    customFingerprint = { methodDef ->
-        methodDef.definingClass.endsWith("BuildConfigUtil;") && methodDef.name == "shouldShowDebugOptions"
+internal object ShouldShowDebugOptionsFingerprint : MethodFingerprint(
+    customFingerprint = { methodDef, _ ->
+        methodDef.definingClass.endsWith("/BuildConfigUtil;") && methodDef.name == "shouldShowDebugOptions"
     }
 )
--- src/main/kotlin/app/revanced/patches/twitter/misc/hook/json/fingerprints/JsonInputStreamFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/twitter/misc/hook/json/fingerprints/JsonInputStreamFingerprint.kt
@@ -1,9 +1,9 @@
 package app.revanced.patches.twitter.misc.hook.json.fingerprints
 
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import app.revanced.patcher.fingerprint.MethodFingerprint
 
-object JsonInputStreamFingerprint : MethodFingerprint(
-    customFingerprint = { methodDef ->
+internal object JsonInputStreamFingerprint : MethodFingerprint(
+    customFingerprint = { methodDef, _ ->
         if (methodDef.parameterTypes.size == 0) false
         else methodDef.parameterTypes.first() == "Ljava/io/InputStream;"
     }
