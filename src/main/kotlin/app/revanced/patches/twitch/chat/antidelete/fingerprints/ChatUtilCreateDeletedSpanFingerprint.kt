74d8e6b9c523742177807749aa66e9078b0ff262
Mafiadoener36 <Mafiadoener36@users.noreply.github.com>
2024-05-15 01:19:54 +0000

    Automated debugMerge

--- src/main/kotlin/app/revanced/patches/twitch/chat/antidelete/fingerprints/ChatUtilCreateDeletedSpanFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/twitch/chat/antidelete/fingerprints/ChatUtilCreateDeletedSpanFingerprint.kt
@@ -1,9 +1,9 @@
 package app.revanced.patches.twitch.chat.antidelete.fingerprints
 
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import app.revanced.patcher.fingerprint.MethodFingerprint
 
-object ChatUtilCreateDeletedSpanFingerprint : MethodFingerprint(
-    customFingerprint = { methodDef ->
+internal object ChatUtilCreateDeletedSpanFingerprint : MethodFingerprint(
+    customFingerprint = { methodDef, _ ->
         methodDef.definingClass.endsWith("/ChatUtil\$Companion;") && methodDef.name == "createDeletedSpanFromChatMessageSpan"
     }
 )
--- src/main/kotlin/app/revanced/patches/twitch/chat/antidelete/fingerprints/SetHasModAccessFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/twitch/chat/antidelete/fingerprints/SetHasModAccessFingerprint.kt
@@ -1,10 +1,10 @@
 package app.revanced.patches.twitch.chat.antidelete.fingerprints
 
 
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import app.revanced.patcher.fingerprint.MethodFingerprint
 
-object SetHasModAccessFingerprint : MethodFingerprint(
-    customFingerprint = { methodDef ->
+internal object SetHasModAccessFingerprint : MethodFingerprint(
+    customFingerprint = { methodDef, _ ->
         methodDef.definingClass.endsWith("DeletedMessageClickableSpan;") && methodDef.name == "setHasModAccess"
     }
 )
