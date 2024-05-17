91a4e48726de582861ae7cfb4a6e1eb280d920f6
Mafiadoener36 <Mafiadoener36@users.noreply.github.com>
2024-05-17 01:19:46 +0000

    Automated debugMerge

--- src/main/kotlin/app/revanced/patches/twitch/chat/antidelete/fingerprints/ChatUtilCreateDeletedSpanFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/twitch/chat/antidelete/fingerprints/ChatUtilCreateDeletedSpanFingerprint.kt
@@ -1,9 +1,9 @@
 package app.revanced.patches.twitch.chat.antidelete.fingerprints
 
-import app.revanced.patcher.fingerprint.MethodFingerprint
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 
-internal object ChatUtilCreateDeletedSpanFingerprint : MethodFingerprint(
-    customFingerprint = { methodDef, _ ->
+object ChatUtilCreateDeletedSpanFingerprint : MethodFingerprint(
+    customFingerprint = { methodDef ->
         methodDef.definingClass.endsWith("/ChatUtil\$Companion;") && methodDef.name == "createDeletedSpanFromChatMessageSpan"
     }
 )
