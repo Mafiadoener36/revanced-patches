1373987b4f595d5c2095f0179828e70056702efc
Mafiadoener36 <Mafiadoener36@users.noreply.github.com>
2024-06-15 01:22:33 +0000

    Automated debugMerge

--- src/main/kotlin/app/revanced/patches/twitch/debug/fingerprints/IsOmVerificationEnabledFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/twitch/debug/fingerprints/IsOmVerificationEnabledFingerprint.kt
@@ -1,9 +1,10 @@
 package app.revanced.patches.twitch.debug.fingerprints
 
-import app.revanced.patcher.fingerprint.MethodFingerprint
 
-internal object IsOmVerificationEnabledFingerprint : MethodFingerprint(
-    customFingerprint = { methodDef, _ ->
-        methodDef.definingClass.endsWith("/BuildConfigUtil;") && methodDef.name == "isOmVerificationEnabled"
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+
+object IsOmVerificationEnabledFingerprint : MethodFingerprint(
+    customFingerprint = { methodDef ->
+        methodDef.definingClass.endsWith("BuildConfigUtil;") && methodDef.name == "isOmVerificationEnabled"
     }
 )
