23e92a76ac109738a91b839dc7c704cf2966765a
Mafiadoener36 <Mafiadoener36@users.noreply.github.com>
2024-06-19 01:23:52 +0000

    Automated debugMerge

--- src/main/kotlin/app/revanced/patches/tiktok/misc/login/disablerequirement/fingerprints/MandatoryLoginServiceFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/tiktok/misc/login/disablerequirement/fingerprints/MandatoryLoginServiceFingerprint.kt
@@ -1,8 +1,8 @@
 package app.revanced.patches.tiktok.misc.login.disablerequirement.fingerprints
 
-import app.revanced.patcher.fingerprint.MethodFingerprint
-internal object MandatoryLoginServiceFingerprint : MethodFingerprint(
-    customFingerprint = { methodDef, _ ->
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+object MandatoryLoginServiceFingerprint : MethodFingerprint(
+    customFingerprint = { methodDef ->
         methodDef.definingClass.endsWith("/MandatoryLoginService;") &&
                 methodDef.name == "enableForcedLogin"
     }
