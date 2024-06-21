77802f1a6e8deff239b73e50028a3e14b0128a41
Mafiadoener36 <Mafiadoener36@users.noreply.github.com>
2024-06-21 01:22:04 +0000

    Automated debugMerge

--- src/main/kotlin/app/revanced/patches/twitch/misc/settings/fingerprints/MenuGroupsUpdatedFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/twitch/misc/settings/fingerprints/MenuGroupsUpdatedFingerprint.kt
@@ -1,9 +1,9 @@
 package app.revanced.patches.twitch.misc.settings.fingerprints
 
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import app.revanced.patcher.fingerprint.MethodFingerprint
 
-object MenuGroupsUpdatedFingerprint : MethodFingerprint(
-    customFingerprint = { methodDef ->
+internal object MenuGroupsUpdatedFingerprint : MethodFingerprint(
+    customFingerprint = { methodDef, _ ->
         methodDef.definingClass.endsWith("/SettingsMenuPresenter\$Event\$MenuGroupsUpdated;")
                 && methodDef.name == "<init>"
     }
