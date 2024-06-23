20ab6943b29b4a3ae88da84446a63d547f084b89
Mafiadoener36 <Mafiadoener36@users.noreply.github.com>
2024-06-23 01:33:47 +0000

    Automated debugMerge

--- src/main/kotlin/app/revanced/patches/tiktok/misc/settings/fingerprints/AdPersonalizationActivityOnCreateFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/tiktok/misc/settings/fingerprints/AdPersonalizationActivityOnCreateFingerprint.kt
@@ -1,9 +1,9 @@
 package app.revanced.patches.tiktok.misc.settings.fingerprints
 
-import app.revanced.patcher.fingerprint.MethodFingerprint
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 
-internal object AdPersonalizationActivityOnCreateFingerprint : MethodFingerprint(
-    customFingerprint = { methodDef, _ ->
+object AdPersonalizationActivityOnCreateFingerprint : MethodFingerprint(
+    customFingerprint = { methodDef ->
         methodDef.definingClass.endsWith("/AdPersonalizationActivity;") &&
                 methodDef.name == "onCreate"
     }
